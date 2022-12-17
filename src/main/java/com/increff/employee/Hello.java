c2c366a4f94c4b2b2d90013402e414f1c19a08b3
Aayush Maurya <aayushmaurya1099@gmail.com>
2022-12-18 02:19:49 +0530

    connecting to database

--- src/main/java/com/increff/employee/Hello.java
+++ src/main/java/com/increff/employee/Hello.java
@@ -1,4 +1,29 @@
 package com.increff.employee;
 
+import java.sql.*;
+
 public class Hello {
+    public static void main(String[] args) throws SQLException
+    {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "Aayush", "Aayush10m");
+            Statement stmt = con.createStatement();
+            ResultSet res = stmt.executeQuery("select * from employees");
+            while(res.next())
+            {
+                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getInt(3));
+            }
+            stmt.executeUpdate("delete from employees where id=3");
+            res = stmt.executeQuery("select * from employees");
+            while(res.next())
+            {
+                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getInt(3));
+            }
+            con.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
