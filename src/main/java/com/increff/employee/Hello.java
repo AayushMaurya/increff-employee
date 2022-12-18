14a8cbd498f51355b395f43143c7f22bb482da9f
Aayush Maurya <aayushmaurya1099@gmail.com>
2022-12-18 14:44:23 +0530

    java properties added

--- src/main/java/com/increff/employee/Hello.java
+++ src/main/java/com/increff/employee/Hello.java
@@ -1,29 +1,73 @@
 package com.increff.employee;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class Hello {
-    public static void main(String[] args) throws SQLException
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException
     {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee", "Aayush", "Aayush10m");
-            Statement stmt = con.createStatement();
-            ResultSet res = stmt.executeQuery("select * from employees");
-            while(res.next())
-            {
-                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getInt(3));
-            }
-            stmt.executeUpdate("delete from employees where id=3");
-            res = stmt.executeQuery("select * from employees");
-            while(res.next())
-            {
-                System.out.println(res.getInt(1) + " " + res.getString(2) + " " + res.getInt(3));
-            }
+        Properties props = new Properties();
+        InputStream inStream = new FileInputStream("employee.properties");
+        props.load(inStream);
+
+            Class.forName(props.getProperty("jdbc.driver"));
+            Connection con = DriverManager.getConnection(props.getProperty("jdbc.url"), props.getProperty("jdbc.user"),
+                    props.getProperty("jdbc.password"));
+
+            insert(con);
+            select(con);
+            delete(con);
+
             con.close();
+    }
+
+    private static void select(Connection con) throws SQLException
+    {
+        System.out.println("Selecting Employee");
+        Statement stmt = con.createStatement();
+        ResultSet res = stmt.executeQuery("select * from employees");
+        while(res.next())
+        {
+            System.out.println(res.getInt(1) + "  " + res.getString(2) + "  " + res.getInt(3));
         }
-        catch (Exception e) {
-            System.out.println(e);
+        stmt.close();
+    }
+
+    private static void insert(Connection con) throws SQLException
+    {
+        System.out.println("Inserting into employees");
+            Statement stmt = con.createStatement();
+            for(int i=5; i<8; i++)
+            {
+                int id = i;
+                int age = 20 + i;
+                String name = "user" + i;
+                stmt.executeUpdate("insert into employees values ( " + id + ",'" + name + "', " + age + ");");
+            }
+            stmt.close();
+    }
+
+    private static void delete(Connection con) throws SQLException
+    {
+        System.out.println("Deleting Employee");
+        Statement stmt = con.createStatement();
+        ResultSet res = stmt.executeQuery("select * from employees");
+
+        List<Integer> idList = new ArrayList<Integer>();
+        while(res.next())
+        {
+            idList.add(res.getInt(1));
         }
+        for(int i=0; i<idList.size(); i++)
+        {
+            stmt.executeUpdate("delete from employees where id=" + idList.get(i) + ";");
+        }
+        stmt.close();
     }
 }
